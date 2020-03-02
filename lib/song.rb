15dee4bec18113c55100650735cb7fc41eaaadf7
Jake Garcia <bballwiz4@gmail.com>
2020-03-02 21:38:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,8 @@ class Song
 
   def self.new_by_filename(filename)
     filename_array = filename.split(" - ")
-    print filename_array
+    artist = Artist.find_or_create_by_name(filename_array[0])
+
   end
 
 end
